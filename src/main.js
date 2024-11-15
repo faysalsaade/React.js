927e1c88956ba6ce10c882afb98dff830e9d303d
faysalsaade <faysalsaadeh@gmail.com>
2024-11-15 19:56:10 +0200

    create new project

--- src/main.js
+++ src/main.js
@@ -7,13 +7,16 @@
 //     .then((data) => data.map((user) => console.log(user.name, user.address)))
 //     .catch((Error) => console.log(console.error("Error 404")))
 // );
+
 // Second Way
 
-async function get() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
-  data.map((user) => console.log(user.name, user.address));
-}
-get()
-  .then((response) => console.log("api fetched"))
-  .catch((error) => console.error("Error 404"));
+// async function get() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const data = await response.json();
+//   data.map((user) => console.log(user.name, user.address));
+// }
+// get()
+//   .then((response) => console.log("api fetched"))
+//   .catch((error) => console.error("Error 404"));
+
+// Third Way
