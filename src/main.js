0f96e07f6bd8038becacd2fce7f562c353a40899
faysalsaade <faysalsaadeh@gmail.com>
2024-11-15 19:44:05 +0200

    create new project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-// fetch
+// Fetch
 
-// first way
-fetch("https://jsonplaceholder.typicode.com/users");
+// First Way
+// fetch("s").then((response) =>
+//   response
+//     .json()
+//     .then((data) => data.map((user) => console.log(user.name, user.address)))
+//     .catch((Error) => console.log(console.error("Error 404")))
+// );
+// Second Way
+
+async function get() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+}
+get();
