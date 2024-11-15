eb05e566921607b7fdf5860cd359469544fda94c
faysalsaade <faysalsaadeh@gmail.com>
2024-11-15 19:53:52 +0200

    create new project

--- src/main.js
+++ src/main.js
@@ -11,5 +11,9 @@
 
 async function get() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  data.map((user) => console.log(user.name, user.address));
 }
-get();
+get()
+  .then((response) => console.log("api fetched"))
+  .catch((error) => console.error("Error 404"));
