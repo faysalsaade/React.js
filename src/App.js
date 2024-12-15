12ef2604e70af95e800f07ee82eb21a8e69cc8b6
faysalsaade <faysalsaadeh@gmail.com>
2024-12-15 22:36:00 +0200

    create new project

--- src/App.js
+++ src/App.js
@@ -66,21 +66,7 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 const App = () => {
   const [posts, setpost] = useState([]);
-  const [id,setId] useState([])
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        setpost(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
-  return (
-    <div>
-      <ul>
-        <input type="text"></input>
+  const [id,setId] useState([])="text"></input>
         {posts.map((post) => (
           <li>{post.title}</li>
         ))}
