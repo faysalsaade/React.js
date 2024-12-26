04ab193afb6b8164509b6645027bd82c2b11b3e2
faysalsaade <faysalsaadeh@gmail.com>
2024-12-26 16:41:48 +0200

    create new project

--- src/ex.js
+++ src/ex.js
@@ -3,3 +3,4 @@ const reducer = (accumaltor, currentvalue) => accumaltor + currentvalue;
 
 console.log(array1.reduce(reducer));
 console.log("#".repeat(70));
+console.log(array1.reduce(reducer, 5));
