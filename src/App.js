9199229aafc908afbf9b5bdb228909788acefdc3
faysalsaade <faysalsaadeh@gmail.com>
2024-12-27 18:56:56 +0200

    create new project

--- src/App.js
+++ src/App.js
@@ -106,19 +106,8 @@
 // export default App;
 
 import { React, useReducer } from "react";
-const initialstate = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    case "reset":
-      return initialstate;
-    default:
-      return state;
-  }
-};
+import reducer from "./reducer";
+import { initialstate } from "./reducer";
 const App = () => {
   const [count, dispatch] = useReducer(reducer, initialstate);
   return (
---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+import React from "react";
+export const initialstate = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialstate;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
