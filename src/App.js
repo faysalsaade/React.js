01e286c81b0ecc5b09b40f9a0ffa7c4c3db90fbc
faysalsaade <faysalsaadeh@gmail.com>
2024-12-27 14:28:13 +0200

    create new project

--- src/App.js
+++ src/App.js
@@ -107,14 +107,26 @@
 
 import { React, useReducer } from "react";
 const initialstate = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialstate;
+    default:
+      return state;
+  }
+};
 const App = () => {
   const [count, dispatch] = useReducer(reducer, initialstate);
   return (
     <div>
-      <h1>count - {count}</h1>
-      <button>increment</button>
-      <button>decrement</button>
-      <button>reset</button>
+      <h1>count : {count}</h1>
+      <button onClick={() => dispatch("increment")}>increment </button>
+      <button onClick={() => dispatch("decrement")}>decrement</button>
+      <button onClick={() => dispatch("reset")}>reset</button>
     </div>
   );
 };
